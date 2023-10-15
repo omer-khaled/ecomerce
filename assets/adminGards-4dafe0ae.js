8ab38fabd7f41774df749a4aac6648dee1b782a6
omer-khaled <omer-khaled@users.noreply.github.com>
2023-10-15 13:54:10 +0000

    deploy: c160f2c34db5846d4eaa117d69b3d02b463ef85e

---
+++ assets/adminGards-4dafe0ae.js
@@ -0,0 +1,1 @@
+import{d as a,j as t}from"./index-594e7971.js";import e from"./Login-354816f1.js";const u=function(r){function o(){const n=a(s=>s.auth.auth);return t.jsx(t.Fragment,{children:n!==null&&n===!0?t.jsx(r,{}):t.jsx(e,{admin:!0})})}return o},c=u;export{c as a};
