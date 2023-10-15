86487b563112493616142a73f95cee6018093d29
omer-khaled <omer-khaled@users.noreply.github.com>
2023-10-15 17:58:41 +0000

    deploy: 19b77b35ce66296ca01c24702b3185331d4ec885

---
+++ assets/modal-ce0b874b.js
@@ -0,0 +1,1 @@
+import{S as t}from"./sweetalert2.all-7a2e22d8.js";const c=(i,e,r)=>i?t.fire({title:`${e} Successfully!`,text:r,icon:"success"}):t.fire({title:`${e} Faild!`,text:r,icon:"error"});export{c as m};
