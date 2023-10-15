d28d1fcf525611f716a852041d88251aaa0236f4
omer-khaled <omer-khaled@users.noreply.github.com>
2023-10-15 14:20:38 +0000

    deploy: a61e33bcecd24aa1bc1a085daf3347b91930a530

--- assets/modal-17cec2b5.js
+++
@@ -1,1 +0,0 @@
-import{S as t}from"./sweetalert2.all-adf8d552.js";const c=(i,e,r)=>i?t.fire({title:`${e} Successfully!`,text:r,icon:"success"}):t.fire({title:`${e} Faild!`,text:r,icon:"error"});export{c as m};
---
+++ assets/modal-590cce2f.js
@@ -0,0 +1,1 @@
+import{S as t}from"./sweetalert2.all-343df95c.js";const c=(i,e,r)=>i?t.fire({title:`${e} Successfully!`,text:r,icon:"success"}):t.fire({title:`${e} Faild!`,text:r,icon:"error"});export{c as m};
