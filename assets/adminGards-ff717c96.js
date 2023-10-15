43e7fc3244573cb013a6be2603c6de2e970445f4
omer-khaled <omer-khaled@users.noreply.github.com>
2023-10-15 14:13:04 +0000

    deploy: 60221a407fb9013fd4b56a8c5644f4d52f68b443

---
+++ assets/adminGards-ff717c96.js
@@ -0,0 +1,1 @@
+import{d as a,j as t}from"./index-1da75941.js";import e from"./Login-c37a4fb2.js";const u=function(r){function o(){const n=a(s=>s.auth.auth);return t.jsx(t.Fragment,{children:n!==null&&n===!0?t.jsx(r,{}):t.jsx(e,{admin:!0})})}return o},c=u;export{c as a};
---
+++ assets/modal-17cec2b5.js
@@ -0,0 +1,1 @@
+import{S as t}from"./sweetalert2.all-adf8d552.js";const c=(i,e,r)=>i?t.fire({title:`${e} Successfully!`,text:r,icon:"success"}):t.fire({title:`${e} Faild!`,text:r,icon:"error"});export{c as m};
